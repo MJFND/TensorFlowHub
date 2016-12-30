8bd4aa4584d063af898fa0b1585e0421c760b864
Junaid Effendi <junaidfnd@gmail.com>
2016-12-30 21:17:05 +0500

    Update nn_multi_hidden_layer_l2_dropout_decay.py

--- nn_multi_hidden_layer_l2_dropout_decay.py
+++ nn_multi_hidden_layer_l2_dropout_decay.py
@@ -1,3 +1,5 @@
+#from www.tensorflowhub.org
+
 from __future__ import print_function
 import numpy as np
 import tensorflow as tf
@@ -34,49 +36,56 @@ test_dataset, test_labels = reformat(test_dataset, test_labels)
 print('Training set', train_dataset.shape, train_labels.shape)
 print('Validation set', valid_dataset.shape, valid_labels.shape)
 print('Test set', test_dataset.shape, test_labels.shape)
-
 # With gradient descent training, even this much data is prohibitive.
 # Subset the training data for faster turnaround.
 train_subset = 10000
+batch_size = 100
 
-batch_size = 128
+def Neural_Network(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    hidden_layer_1 = tf.nn.relu(layer_1)
+    layer_2 = tf.add(tf.matmul(hidden_layer_1, weights['h2']), biases['b2'])
+    hidden_layer_2 = tf.nn.relu(layer_2)
+    layer_3 = tf.add(tf.matmul(hidden_layer_2, weights['h3']), biases['b3'])
+    hidden_layer_3 = tf.nn.relu(layer_3)
+    #Output layer with linear activation
+    out_layer = tf.matmul(hidden_layer_3, weights['out']) + biases['out']
+    return out_layer
 
-
-def multilayer_perceptron(x, weights, biases):
+	
+def Neural_Network_Dropout(x, weights, biases):
     keep_prob = tf.Variable(0.5,tf.float32)
     # Hidden layer with RELU activation
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-    drop_1 = tf.nn.dropout(layer_1,keep_prob)
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
-    drop_2 = tf.nn.dropout(layer_2,keep_prob)
-    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
-    layer_3 = tf.nn.relu(layer_3)
-    drop_3 = tf.nn.dropout(layer_3,keep_prob)
+    hidden_layer_1 = tf.nn.relu(layer_1)
+    drop_1 = tf.nn.dropout(hidden_layer_1,keep_prob)
+    layer_2 = tf.add(tf.matmul(drop_1, weights['h2']), biases['b2'])
+    hidden_layer_2 = tf.nn.relu(layer_2)
+    drop_2 = tf.nn.dropout(hidden_layer_2,keep_prob)
+    layer_3 = tf.add(tf.matmul(drop_2, weights['h3']), biases['b3'])
+    hidden_layer_3 = tf.nn.relu(layer_3)
+    drop_3 = tf.nn.dropout(hidden_layer_3,keep_prob)
     #Output layer with linear activation
-    out_layer = tf.matmul(layer_3, weights['out']) + biases['out']
-    
+    out_layer = tf.matmul(drop_3, weights['out']) + biases['out']
     return out_layer
-
-
+	
 graph = tf.Graph()
 with graph.as_default():
 
   # Input data. For the training data, we use a placeholder that will be fed
   # at run time with a training minibatch.
-  tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size * image_size))
+  tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, 784))
   tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
   tf_valid_dataset = tf.constant(valid_dataset)
   tf_test_dataset = tf.constant(test_dataset)
   
   n_hidden_1 = 1024 # 1st layer number of features
-  n_hidden_2 = 1024 # 1st layer number of features
-  n_hidden_3 = 205 # 1st layer number of features
+  n_hidden_2 = 300 # 1st layer number of features
+  n_hidden_3 = 50 # 1st layer number of features
   n_input = 784 # MNIST data input (img shape: 28*28)
   n_classes = 10 # MNIST total classes (0-9 digits)
   # Training computation.
-    
   
 
   #Store layers weight & bias
@@ -94,29 +103,23 @@ with graph.as_default():
     'out': tf.Variable(tf.random_normal([n_classes]))
   }
 
-# Construct model
-  logits = multilayer_perceptron(tf_train_dataset, weights, biases)
+  #Construct model
+  logits = Neural_Network_Dropout(tf_train_dataset, weights, biases)
 
-
-
-  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels)+ 0.01 * tf.nn.l2_loss(weights['h1']) + 0.01 * tf.nn.l2_loss(weights['h2'])  + 0.01 * tf.nn.l2_loss(weights['h3']) + 0.01 * tf.nn.l2_loss(weights['out']))
+  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels)+ 0.03 * tf.nn.l2_loss(weights['h1']) + 0.03 * tf.nn.l2_loss(weights['h2'])  + 0.03 * tf.nn.l2_loss(weights['h3']) + 0.03 * tf.nn.l2_loss(weights['out']))
+    
   
-
   global_step = tf.Variable(0)  # count the number of steps taken.
   learning_rate = tf.train.exponential_decay(0.3, global_step, 3500, 0.86, staircase=True)
   optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
- 
+  
   
   # Predictions for the training, validation, and test data.
   train_prediction = tf.nn.softmax(logits)
-  logits1 = multilayer_perceptron(tf_valid_dataset, weights, biases)
+  logits1 = Neural_Network(tf_valid_dataset, weights, biases)
   valid_prediction = tf.nn.softmax(logits1)
-  logits2 = multilayer_perceptron(tf_test_dataset, weights, biases)
+  logits2 = Neural_Network(tf_test_dataset, weights, biases)
   test_prediction = tf.nn.softmax(logits2)
-
-
-
-
 
 num_steps = 12001
 
@@ -142,14 +145,10 @@ with tf.Session(graph=graph) as session:
     
     _, l, predictions = session.run(
       [optimizer, loss, train_prediction], feed_dict=feed_dict)
-    if (step % 500 == 0):
+    if (step % 2000 == 0):
       print("Minibatch loss at step %d: %f" % (step, l))
       print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
       print("Validation accuracy: %.1f%%" % accuracy(
         valid_prediction.eval(), valid_labels))
       #print(weights['out'].eval())
   print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
-
-
-
-
